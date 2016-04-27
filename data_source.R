41d85d1758fac6ab96066c5421b912a58e049c83
TenthSpeedWriter (Adam L. Seale) <addseale2@gmail.com>
2016-04-27 18:53:01 -0500

    Adds script to draw from, tidy, and organize source data

---
+++ data_source.R
@@ -0,0 +1,31 @@
+library(dplyr)
+source("age_in_months.R")
+
+load_data_source <- function(path="train.csv",
+                             method=read.csv) {
+  raw <- method(path)
+  
+  transmute(raw,
+            id = AnimalID,
+            # Factor variable: cat or dog.
+            species = AnimalType,
+                
+            # Factor variables, target: Adopted, passed away, euthanized,
+            # returned to owner, or transferred.
+            outcome_type = OutcomeType,
+            
+            # Factor variable, many levels: Creature's specific breed
+            breed = Breed,
+                
+            # Factor variable, many levels: Coat color
+            color = Color,
+            
+            # Factor: Levels depend on outcome_type.
+            # outcome_subtype = OutcomeSubtype,
+            
+            # Factor: Five levels for intact/fixed male/female, plus "Unknown".
+            sex_at_outcome = SexuponOutcome,
+            
+            # Numeric: Age in months, estimating by 1/30th-month days and 1/4th-month weeks
+            age_at_outcome = age_in_months(AgeuponOutcome))
+}
